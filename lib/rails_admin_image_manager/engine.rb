3d850075ef4cea74430196bd6c7d9584907274db
Mario <mario@ixmedia.com>
2017-06-27 09:34:45 -0400

    RailsAdmin initializer reload function.

--- lib/rails_admin_image_manager/engine.rb
+++ lib/rails_admin_image_manager/engine.rb
@@ -1,5 +1,27 @@
 module RailsAdminImageManager
   class Engine < ::Rails::Engine
     isolate_namespace RailsAdminImageManager
+
+    initializer :reload_rails_admin_configs do |app|
+      Rails.application.config.to_prepare do
+        RailsAdmin::ApplicationController.class_eval do
+          before_action :reload_rails_admin_image_manager, if: :reload_rails_image_manager? # Reloading RailsAdmin Config Automatically
+
+          def reload_rails_admin_image_manager
+            gem_dir = Gem::Specification.find_by_name("rails_admin_image_manager").gem_dir
+
+            Dir.foreach("#{gem_dir}/config/initializers/rails_admin") do |item|
+              next if item == '.' or item == '..'
+              load("#{gem_dir}/config/initializers/rails_admin/#{item}")
+            end
+          end
+
+          def reload_rails_image_manager?
+            Rails.env.development?
+          end
+        end
+      end
+    end
+
   end
 end
