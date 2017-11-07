cb8e9c379778fee10b4d0b396141f7de2d9bd30f
Lπ <louis.philippe.dumas@gmail.com>
2017-11-07 10:46:40 -0500

    fix image src in railsAdmin store insertImage method

--- app/assets/javascripts/rails_admin_image_manager/stores/railsAdmin.js
+++ app/assets/javascripts/rails_admin_image_manager/stores/railsAdmin.js
@@ -18,7 +18,7 @@ const railsAdmin = {
       // Generating new dynamic image size before sending it back
       axios.get(`/images/${idImage}?width=0&height=0`).then((response) => {
         dispatch('overlayStore/showProgressOverlay', false, {root:true})
-        window.opener.insertImageManagerMedia(idImage, state.inputId, response.data.src_for_wysiwyg)
+        window.opener.insertImageManagerMedia(idImage, state.inputId, response.data.image.src_for_wysiwyg)
         window.close()
       })
     }
